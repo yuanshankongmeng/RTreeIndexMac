00df0ffa36236b5e66d7971260370f05b787b725
zhou dong <zhoudongenator@gmail.com>
2011-09-27 09:40:40 +0800

    Initial Commit

---
+++ RTreeIndexMacTests/RTreeIndexMacTests.h
@@ -0,0 +1,17 @@
+//
+//  RTreeIndexMacTests.h
+//  RTreeIndexMacTests
+//
+//  Created by zhoudong on 11-9-27.
+//  Copyright 2011年 tg. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface RTreeIndexMacTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ RTreeIndexMacTests/RTreeIndexMacTests.m
@@ -0,0 +1,33 @@
+//
+//  RTreeIndexMacTests.m
+//  RTreeIndexMacTests
+//
+//  Created by zhoudong on 11-9-27.
+//  Copyright 2011年 tg. All rights reserved.
+//
+
+#import "RTreeIndexMacTests.h"
+
+
+@implementation RTreeIndexMacTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RTreeIndexMacTests");
+}
+
+@end
